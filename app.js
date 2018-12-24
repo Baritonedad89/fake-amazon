3a4cd6a2d2969c7a559c3a8066280a400930ee42
Hero's Dad <cody.monta@gmail.com>
2018-12-23 22:33:01 -0600

    great work on manager's portal

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let value = "cody4";
-const func = () =>{
-for (let i = 0; i < value.length; i++) {
-    console.log(value[i])
+const func = () => {
 
-    for (let j = 0; j < number.length; j++) {
+        for (let i = 0; i < value.length; i++) {
+            return value[i];
+            console.log(value[i])
 
-        // if (value[i] == number[j]) {
-        //     return true;
-        // } else {
-        //     return false;
+            for (let j = 0; j < number.length; j++) {
 
-        // }
+                if (value[i] === number[j]) {
+                    return true;
+                }
 
+            }
+            // return false;
+
+        }
     }
 
-}
-
-}
 console.log(func())
--- bamazonManager.js
+++ bamazonManager.js
@@ -13,18 +13,19 @@ const connection = mysql.createConnection({
 const makeConnection = () => {
     connection.connect(err => {
         if (err) throw err;
-        console.log("connected as id " + connection.threadId + "\n");
+        // console.log("connected as id " + connection.threadId + "\n");
     });
 };
 
 
 const createManager = () => {
-    makeConnection();
-    console.log("\nWelcome to the manager portal\n");
+    console.log("\nWelcome to the Manager Profile Creation Portal\n");
     setTimeout(prompt, 500)
 };
 
-const prompt = () => {
+const create = () => {
+    console.log("\nLet's set you up for a new Manager profile");
+    console.log('-------------------------------------------')
     inquirer.prompt([
         {
             message: "Please enter your first name:",
@@ -98,14 +99,117 @@ const prompt = () => {
                     console.log(`\nNew Manager profile created!\nThank you, ${response.firstName}!`)
                 }
             )
+            setTimeout(managerPortal, 1000)
         }
 
     })
     // use these credentials to sign in to the portal and run that function 
 };
-createManager();
-
 
 const signIntoPortal = () => {
+    console.log("\nSign into Manager's Portal");
+    console.log('-------------------------------------------')
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter your user name",
+            name: "userName",
+            validate: function (value) {
+                if (value.length <= 5) {
+                    console.log('\nUsername must be at least 6 characters long')
+                    return false;
+                } return true;
+            }
+        },
+        {
+            type: "password",
+            mask: '*',
+            message: "Enter your password",
+            name: "password",
+            validate: function (value) {
+                if (value.length < 8) {
+                    console.log('\nPassword must be at least 8 characters long')
+                    return false;
+                } return true;
+            }
+        }
+    ]).then(response => {
+        const userName = response.userName;
+        const password = response.password;
+        let query = `SELECT first_name, last_name FROM logins WHERE user_name ="${userName}" AND password ="${password}"`;
+
+        connection.query(query, function (err, res) {
+            if (err) throw err;
+            if (res.length < 1) {
+                console.log('\nLogin credentials invalid.\nPlease check either user name or password and try again.\n');
+                signIntoPortal()
+            } else {
+                for (let i = 0; i < res.length; i++) {
+                    const validUser = res[i];
+                    console.log('-------------------------------------------')
+                    console.log(`\n${validUser.first_name} ${validUser.last_name} signed in.\n`)
+                }
+            }
+        });
+        // do something after use is signed in 
+    }).then(response => {
+        inquirer.prompt(
+            {
+                message: "Menu Options:",
+                type: "list",
+                name: "menu",
+                choices: [
+                    "View Products for Sale",
+                    "View Low Inventory",
+                    "Add to Inventory",
+                    "Add New Product",
+                    "Exit Portal"
+                ]
+            }).then(response => {
+                switch (response.menu) {
+                    case "View Products for Sale":
+                        // run this function
+                        break;
+                    case "View Low Inventory":
+                        // run this function
+                        break;
+                    case "Add to Inventory":
+                        // run this function
+                        break;
+                    case "Add New Product":
+                        // run this function
+                        break;
+                    case "Exit Portal":
+                        connection.end();
+                        break;
+                }
+            })
+    })
 
 }
+const managerPortal = () => {
+    makeConnection();
+    console.log("Welcome to the Manager Portal.");
+    console.log('---------------------------------');
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "option",
+            choices: ["Create a new Manager profile", "Sign into portal", "Exit Portal"]
+        }
+    ]).then(response => {
+        switch (response.option) {
+            case "Create a new Manager profile":
+                create();
+                break;
+            case "Sign into portal":
+                signIntoPortal();
+                break
+            case "Exit Portal":
+                connection.end();
+                break;
+        }
+    })
+}
+managerPortal()
